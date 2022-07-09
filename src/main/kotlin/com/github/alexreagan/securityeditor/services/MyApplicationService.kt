7a659b2750187ec7abc6dd3adb0b60fc44908b07
GitHub Action <action@github.com>
2022-07-09 00:11:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/alexreagan/securityeditor/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/alexreagan/securityeditor/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.alexreagan.securityeditor.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alexreagan.securityeditor.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/alexreagan/securityeditor/services/MyProjectService.kt
+++ src/main/kotlin/com/github/alexreagan/securityeditor/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.alexreagan.securityeditor.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alexreagan.securityeditor.MyBundle
 
 class MyProjectService(project: Project) {
 
